64b2b5030dd342851765c08ab759c4b260100cf2
sjmazer <sjmazer@ucsb.edu>
2022-01-14 10:58:11 -0800

    Created for-loop commands

--- loops/for-loop.sh
+++ loops/for-loop.sh
@@ -1,1 +1,12 @@
-I'm adding a comment to a shell script called for-loop
+# for-loop example
+# we will be creating a program that "deals" values at random
+#!/bin/bash
+cards=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10)
+
+echo "how many cards would you like?"
+read value
+
+for i in $(seq 1 $value);
+do echo ${cards[RANDOM%${#cards[@]}]};
+
+done
