d2c691bc76dd233d35d16e0cb9fc9cfc5a761840
sjmazer <sjmazer@ucsb.edu>
2022-01-14 10:37:50 -0800

    Adding still more about read

--- user-input/ui-example.sh
+++ user-input/ui-example.sh
@@ -1,4 +1,6 @@
-#'read' command prompts the user to input requested information
 echo "What is your name?"
 read name
 echo "Hello $name."
+#the read command requests users input, and we use '$' to recall the variable established by read
+
+
